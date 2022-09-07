6f58ab5f6b95d0dcaad00f5da2e38dd85b87ffc0
Roman Kolychev <demozbox@gmail.com>
2022-09-08 00:20:50 +0300

    Game scene laid out.

--- Call991/Assets/Scripts/UI/ChoiceButtonView.cs
+++ Call991/Assets/Scripts/UI/ChoiceButtonView.cs
@@ -1,13 +1,15 @@
+using System;
 using System.Threading.Tasks;
 using DG.Tweening;
 using TMPro;
 using UniRx;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 namespace UI
 {
-    public class ChoiceButtonView : MonoBehaviour
+    public class ChoiceButtonView : MonoBehaviour, IPointerDownHandler, IPointerEnterHandler, IPointerExitHandler
     {
         public struct Ctx
         {
@@ -18,58 +20,115 @@ namespace UI
             public float slowButtonFadeDuration;
         }
 
-        [SerializeField] private GameObject clickedImage = default;
-        [SerializeField] private Button button = default;
-        [SerializeField] private TextMeshProUGUI clickedText = default;
+        [SerializeField] private GameObject normal = default;
+        [SerializeField] private GameObject hover = default;
+        [SerializeField] private GameObject clicked = default;
+        [SerializeField] private GameObject blocked = default;
+        [Space]
+        [SerializeField] private Color textNormal = default;
+        [SerializeField] private Color textHover = default;
+        [SerializeField] private Color textClicked = default;
+        [SerializeField] private Color textBlocked = default;
+        [Space] 
+        //[SerializeField] private Button button = default;
         [SerializeField] private TextMeshProUGUI buttonText = default;
         [SerializeField] private CanvasGroup canvasGroup = default;
-        
-        private Ctx _ctx;
 
-        public Button Button => button;
+        private Ctx _ctx;
+        private ButtonStates _currentState;
+
         private static bool _isClicked;
 
         public void SetCtx(Ctx ctx)
         {
             _ctx = ctx;
-            
-            button.onClick.AddListener(() => OnClick());
         }
 
-        private void OnClick()
+        private void SwitchButtonState(ButtonStates state, bool checkClicked = false)
         {
-            if (_isClicked) return;
+            if (checkClicked)
+                if (_currentState == ButtonStates.Clicked && state == ButtonStates.Normal)
+                    state = ButtonStates.Clicked;
 
-            _isClicked = true;
-            _ctx.onClickChoiceButton.Execute(_ctx.index);
+            if (state != ButtonStates.Hover)
+                _currentState = state;
+
+            normal?.SetActive(state is ButtonStates.Normal or ButtonStates.Blocked);
+            hover?.SetActive(state == ButtonStates.Hover);
+            clicked?.SetActive(state == ButtonStates.Clicked);
+            blocked?.SetActive(state == ButtonStates.Blocked);
+
+            switch (state)
+            {
+                case ButtonStates.Normal:
+                    buttonText.color = textNormal;
+                    break;
+                case ButtonStates.Hover:
+                    buttonText.color = textHover;
+                    break;
+                case ButtonStates.Clicked:
+                    buttonText.color = textClicked;
+                    break;
+                case ButtonStates.Blocked:
+                    buttonText.color = textBlocked;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(state), state, null);
+            }
         }
 
         public void SetClicked()
         {
-            clickedImage.gameObject.SetActive(true);
-            button.gameObject.SetActive(false);
+            SwitchButtonState(ButtonStates.Clicked);
         }
-        
-        public async void Show(string description)
+
+        public async void Show(string description, bool isBlocked = false)
         {
             _isClicked = false;
-            
-            clickedText.text = description;
             buttonText.text = description;
-            
-            clickedImage.gameObject.SetActive(false);
-            button.gameObject.SetActive(true);
-            
+            SwitchButtonState(ButtonStates.Normal);
+
+            if (isBlocked)
+                SwitchButtonState(ButtonStates.Blocked);
+
             gameObject.SetActive(true);
             canvasGroup.DOFade(1, _ctx.buttonsAppearDuration);
-            await Task.Delay((int)(_ctx.buttonsAppearDuration * 1000));
+            await Task.Delay((int) (_ctx.buttonsAppearDuration * 1000));
         }
 
         public async void Hide(bool slow)
         {
             var duration = slow ? _ctx.slowButtonFadeDuration : _ctx.fastButtonFadeDuration;
             canvasGroup.DOFade(0, duration);
-            await Task.Delay((int)(duration * 1000));
+            await Task.Delay((int) (duration * 1000));
+        }
+
+        private enum ButtonStates
+        {
+            Normal,
+            Hover,
+            Clicked,
+            Blocked,
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            if(_isClicked) return;
+            
+            SwitchButtonState(ButtonStates.Hover);
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            SwitchButtonState(ButtonStates.Normal, true);
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            if (_isClicked) return;
+            
+            _isClicked = true;
+            _ctx.onClickChoiceButton.Execute(_ctx.index);
         }
     }
 }
