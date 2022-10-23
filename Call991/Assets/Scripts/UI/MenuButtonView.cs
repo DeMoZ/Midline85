00e8cb3aa6f49993441d6fd6a225053a14d54f0c
Roman Kolychev <demozbox@gmail.com>
2022-10-23 13:22:49 +0400

    Main menu arrows keyboard support

--- Call991/Assets/Scripts/UI/MenuButtonView.cs
+++ Call991/Assets/Scripts/UI/MenuButtonView.cs
@@ -5,33 +5,49 @@ using UnityEngine.EventSystems;
 
 namespace UI
 {
-    public class MenuButtonView : MonoBehaviour, IPointerClickHandler, IPointerExitHandler, IPointerEnterHandler
+    public class MenuButtonView : MonoBehaviour, IPointerClickHandler, IPointerExitHandler, IPointerEnterHandler,
+        ISelectHandler, IDeselectHandler
     {
         [SerializeField] private Color textHover = default;
         [SerializeField] private TextMeshProUGUI text = default;
 
         private Color _textColorNormal;
+        private bool _isSelectedByEventSystem;
+
         public event Action OnClick;
 
-        private void Start()
-        {
+        private void Start() =>
             _textColorNormal = text.color;
-        }
 
-        public void OnPointerEnter(PointerEventData eventData)
-        {
-            text.color = textHover;
-        }
+        public void OnPointerEnter(PointerEventData eventData) =>
+            SetHoverColor(true);
 
-        public void OnPointerExit(PointerEventData eventData)
-        {
-            text.color = _textColorNormal;
-        }
+        public void OnPointerExit(PointerEventData eventData) =>
+            SetHoverColor(false);
 
         public void OnPointerClick(PointerEventData eventData)
         {
-            text.color = _textColorNormal;
+            SetHoverColor(false);
             OnClick?.Invoke();
+        }
+
+        public void OnSelect(BaseEventData eventData)
+        {
+            _isSelectedByEventSystem = true;
+            SetHoverColor(true);
+        }
+
+        public void OnDeselect(BaseEventData eventData)
+        {
+            _isSelectedByEventSystem = false;
+            SetHoverColor(false);
+        }
+
+        private void SetHoverColor(bool hover)
+        {
+            text.color = hover ? textHover
+                : _isSelectedByEventSystem ? textHover
+                : _textColorNormal;
         }
     }
 }
