8eb8c96b1c9877d9091456b433439b402068473b
Roman Kolychev <demozbox@gmail.com>
2023-04-20 02:55:07 +0700

    Save / load log to cash.

--- Call991/Assets/Scripts/Core/DialogueLoggerPm.cs
+++ Call991/Assets/Scripts/Core/DialogueLoggerPm.cs
@@ -1,21 +1,30 @@
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using AaDialogueGraph;
 using Newtonsoft.Json;
 using UnityEngine;
 
 /// <summary>
-/// Write all the nodes the player have seen into prefs
+/// Write all the nodes the player have seen into cash and prefs
 /// </summary>
 public class DialogueLoggerPm : IDisposable
 {
     private const string LOGKey = "LogNodes";
     private const string ChoicesKey = "Choices";
     private const string EndsKey = "Ends";
+    private const string CountsKey = "Counts";
 
-    private Dictionary<string, string> _logCash = new();
-    private Dictionary<string, string> _choicesCash = new();
+    private Dictionary<string, string> _logCash;
+    private Dictionary<string, string> _choicesCash;
+    private Dictionary<string, string> _endsCash;
+    private Dictionary<string, int> _countsCash;
+
+    public DialogueLoggerPm()
+    {
+        Load();
+    }
 
     public void AddLog(List<AaNodeData> datas)
     {
@@ -24,7 +33,7 @@ public class DialogueLoggerPm : IDisposable
             AddLog(data);
         }
     }
-    
+
     public void AddLog(AaNodeData data)
     {
         switch (data)
@@ -50,88 +59,75 @@ public class DialogueLoggerPm : IDisposable
 
     private void AddChoice(string choice)
     {
-        var deserializedData = new Dictionary<string, string>();
-        var serializedData = PlayerPrefs.GetString(ChoicesKey, null);
+        Debug.LogError($"[{this}] Add Choice {choice}");
+        _choicesCash.Add(choice, "");
+    }
 
-        if (!string.IsNullOrEmpty(serializedData))
-        {
-            deserializedData = JsonConvert.DeserializeObject<Dictionary<string, string>>(serializedData);
-        }
-
-        deserializedData[choice] = string.Empty;
-        serializedData = JsonConvert.SerializeObject(deserializedData);
-        PlayerPrefs.SetString(ChoicesKey, serializedData);
-        Debug.LogWarning($"[{this}] save one more choice {choice};" +
-                         $"\n{serializedData}");
+    private void AddEnd(string end)
+    {
+        Debug.LogError($"[{this}] Add Choice {end}");
+        _endsCash.Add(end, "");
     }
 
     public bool ContainsChoice(List<string> orChoices)
     {
-        return orChoices.Select(choice => ContainsChoice(choice)).Any(contains => contains);
+        return orChoices.Any(orChoice => _choicesCash.ContainsKey(orChoice));
     }
-    
-    public bool ContainsChoice(string choice)
-    {
-        var deserializedData = new Dictionary<string, string>();
-        var serializedData = PlayerPrefs.GetString(ChoicesKey, null);
 
-        if (!string.IsNullOrEmpty(serializedData))
-        {
-            deserializedData = JsonConvert.DeserializeObject<Dictionary<string, string>>(serializedData);
-        }
-
-        return deserializedData.TryGetValue(choice, out var data);
-    }
-    
     public bool ContainsEnd(List<string> orEnds)
     {
-        return orEnds.Select(end => ContainsEnd(end)).Any(contains => contains);
+        return orEnds.Any(orEnd => _endsCash.ContainsKey(orEnd));
     }
-    
-    public bool ContainsEnd(string end)
-    {
-        var deserializedData = new Dictionary<string, string>();
-        var serializedData = PlayerPrefs.GetString(EndsKey, null);
 
-        if (!string.IsNullOrEmpty(serializedData))
-        {
-            deserializedData = JsonConvert.DeserializeObject<Dictionary<string, string>>(serializedData);
-        }
-
-        return deserializedData.TryGetValue(end, out var data);
-    }
-    
     public void AddCount(string key, int value)
     {
-        var count = PlayerPrefs.GetInt(key, 0);
-        count += value;
-        PlayerPrefs.SetInt(key, count);
+        Debug.LogError($"[{this}] Add Count {key}:{value}");
+        _countsCash.TryGetValue(key, out var count);
+        _countsCash[key] = count + value;
     }
 
     public int GetCount(string key)
     {
-        return PlayerPrefs.GetInt(key, 0);
+        _countsCash.TryGetValue(key, out var count);
+        return count;
     }
-    
+
     private void AddNode(string key, string value)
     {
-        var deserializedData = new Dictionary<string, string>();
-        var serializedData = PlayerPrefs.GetString(LOGKey, null);
-
-        if (!string.IsNullOrEmpty(serializedData))
-        {
-            deserializedData = JsonConvert.DeserializeObject<Dictionary<string, string>>(serializedData);
-        }
-
-        if(deserializedData.TryGetValue(key, out var nodeData))
-        {
-            deserializedData[key] = value;
-            serializedData = JsonConvert.SerializeObject(deserializedData);
-        }
-        
-        PlayerPrefs.SetString(LOGKey,serializedData);
+        _logCash.Add(key, value);
     }
-    
+
+    public void Save()
+    {
+        SetToPrefs(LOGKey, _logCash);
+        SetToPrefs(ChoicesKey, _choicesCash);
+        SetToPrefs(EndsKey, _endsCash);
+        SetToPrefs(CountsKey, _countsCash);
+    }
+
+    private void Load()
+    {
+        _logCash = GetFromPrefs<string>(LOGKey);
+        _choicesCash = GetFromPrefs<string>(ChoicesKey);
+        _endsCash = GetFromPrefs<string>(EndsKey);
+        _countsCash = GetFromPrefs<int>(CountsKey);
+    }
+
+    private Dictionary<string, T> GetFromPrefs<T>(string prefsKey)
+    {
+        var prefsData = PlayerPrefs.GetString(prefsKey, null);
+
+        return !string.IsNullOrEmpty(prefsData)
+            ? JsonConvert.DeserializeObject<Dictionary<string, T>>(prefsData)
+            : new Dictionary<string, T>();
+    }
+
+    private void SetToPrefs<T>(string key, T data)
+    {
+        var serialized = JsonConvert.SerializeObject(data);
+        PlayerPrefs.SetString(key, serialized);
+    }
+
     public void Dispose()
     {
     }
