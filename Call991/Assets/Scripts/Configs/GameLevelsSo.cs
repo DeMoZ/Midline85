9f857dd5d5536d5e352ab56c57685aa0bbc5ee56
Roman Kolychev <demozbox@gmail.com>
2023-08-12 15:29:00 +0700

    Levels sequence group prepared. Added blank L6_Interlude2 dialogue

--- Call991/Assets/Scripts/Configs/GameLevelsSo.cs
+++ Call991/Assets/Scripts/Configs/GameLevelsSo.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using AaDialogueGraph;
 using UnityEngine;
@@ -6,8 +7,17 @@ using UnityEngine;
 public class GameLevelsSo : ScriptableObject
 {
     [SerializeField] private DialogueContainer startButtonLevel;
+    [SerializeField] private List<LevelGroup> levelGroups;
+    
     [SerializeField] private List<DialogueContainer> levels;
     
     public DialogueContainer StartButtonLevel => startButtonLevel;
     public List<DialogueContainer> Levels => levels;
+    public List<LevelGroup> LevelGroups => levelGroups;
+
+    [Serializable]
+    public class LevelGroup
+    {
+        public List<DialogueContainer> Group;
+    }
 }
