debda5fca42b775780f5b30fc0807a1ee4d003ce
Roman Kolychev <demozbox@gmail.com>
2023-09-15 17:08:28 +0700

    unity updated. Newtonsoft json installed.

--- Call991/Assets/AaDialogueGraph/Editor/DialogueGraph.cs
+++ Call991/Assets/AaDialogueGraph/Editor/DialogueGraph.cs
@@ -3,7 +3,6 @@ using System.Linq;
 using Configs;
 using Newtonsoft.Json;
 using UnityEditor;
-using UnityEditor.Experimental.GraphView;
 using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.UIElements;
